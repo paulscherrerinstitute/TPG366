72936c942847b6a49adbeb45930b2a60647ccb57
Dirk Zimoch <dirk.zimoch@psi.ch>
2019-04-05 16:45:41 +0200

    works

--- TPG366.cmd
+++ TPG366.cmd
@@ -1,9 +1,5 @@
 require "stream"
-var streamError 1
-drvAsynIPPortConfigure "$(PORT)","$(IP):8000"
-asynSetOption($(PORT), 0, "disconnectOnReadTimeout", "Y")
-dbLoadTemplate "TPG366.subs","CONTROLLER=$(CONTROLLER),PORT=$(PORT)"
-#var streamDebug 1
-iocInit
-#asynSetTraceMask $(PORT) 0 0x1f
-#asynSetTraceIOMask $(PORT) 0 2
+require "SynApps"
+drvAsynIPPortConfigure "$(PORT=TCP366)","$(IP):8000"
+#asynSetOption($(PORT=TCP366), 0, "disconnectOnReadTimeout", "Y")
+dbLoadTemplate "TPG366.subs","CONTROLLER=$(CONTROLLER=MG1),PORT=$(PORT=TCP366)"
