c81a4a1f74e5b1deb2421e1743b0b3c4c9784006
Dirk Zimoch <dirk.zimoch@psi.ch>
2019-04-08 09:50:42 +0200

    add NNN_ macros for all channels

--- TPG366.cmd
+++ TPG366.cmd
@@ -2,4 +2,4 @@ require "stream"
 require "SynApps"
 drvAsynIPPortConfigure "$(PORT=TCP366)","$(IP):8000"
 #asynSetOption($(PORT=TCP366), 0, "disconnectOnReadTimeout", "Y")
-dbLoadTemplate "TPG366.subs","CONTROLLER=$(CONTROLLER=MG1),PORT=$(PORT=TCP366)"
+dbLoadTemplate "TPG366.subs","CONTROLLER=$(CONTROLLER=MG1),PORT=$(PORT=TCP366),PREC_=-4"
