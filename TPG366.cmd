686df068fce1e0e10725765abaaa3048ddb2838e
Dirk Zimoch <dirk.zimoch@psi.ch>
2019-04-09 09:43:50 +0200

    use more common defaults

--- TPG366.cmd
+++ TPG366.cmd
@@ -1,5 +1,5 @@
 require "stream"
 require "SynApps"
-drvAsynIPPortConfigure "$(PORT=TCP366)","$(IP):8000"
-#asynSetOption($(PORT=TCP366), 0, "disconnectOnReadTimeout", "Y")
-dbLoadTemplate "TPG366.subs","CONTROLLER=$(CONTROLLER=MG1),PORT=$(PORT=TCP366),PREC_=-4"
+drvAsynIPPortConfigure "$(PORT=$(CONTROLLER))","$(IP):8000"
+#asynSetOption($(PORT=$(CONTROLLER)), 0, "disconnectOnReadTimeout", "Y")
+dbLoadTemplate "TPG366.subs","CONTROLLER=$(CONTROLLER),PORT=$(PORT=$(CONTROLLER))"
